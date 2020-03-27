26cdeaa3037cdead2f987fb09f251b24ed813d10
Jesse Belcourt <jessebelcourt@gmail.com>
2020-03-26 21:45:48 -0600

    Stripped comments, and debug code from webpack.mix.js file.

--- src/app/webpack.mix.js
+++ src/app/webpack.mix.js
@@ -1,18 +1,6 @@
 const mix = require('laravel-mix');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-
 mix
   .js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css')
@@ -20,12 +8,10 @@ mix
   .autoload({
     axios: ['axios'],
   })
-  .sourceMaps()
   .version();
 
 if (process.env.APP_DEBUG) {
   console.log("Running in debug mode...");
-  console.log("Working");
   mix.sourceMaps();
 
   mix.webpackConfig({
